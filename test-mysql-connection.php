361458cb1c4299b361783f3b6072e94514a8c6f1
chevanhoang <chevanhoang99@gmail.com>
2023-10-27 13:51:30 +0700

    edit path

--- test-mysql-connection.php
+++ test-mysql-connection.php
@@ -4,7 +4,6 @@ $username = 'hoangcv';
 $password = '12345';
 $database = 'chevanhoang';
 
-// Kết nối đến cơ sở dữ liệu
 $mysqli = new mysqli($host, $username, $password, $database);
 
 if ($mysqli->connect_error) {
@@ -12,13 +11,5 @@ if ($mysqli->connect_error) {
 }
 
 echo 'Kết nối MySQL thành công!';
-
-// Sử dụng mysqldump để đẩy dữ liệu vào luồng đầu ra
-$command = "mysqldump -u {$username} -p{$password} {$database}";
-$output = shell_exec($command);
-
-// In dữ liệu ra màn hình
-echo "<pre>{$output}</pre>";
-
 $mysqli->close();
 ?>
